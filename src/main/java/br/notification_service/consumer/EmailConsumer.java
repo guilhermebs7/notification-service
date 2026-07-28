eb63fd62e7a7d6d490636b368745b7637ac20024
guilhermebs7 <gbs5@cin.ufpe.br>
2026-07-28 19:16:38 -0300

    feat: annotate EmailConsumer class with @Component for Spring management

--- src/main/java/br/notification_service/consumer/EmailConsumer.java
+++ src/main/java/br/notification_service/consumer/EmailConsumer.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailConsumer {
 
     private static final Logger log= LoggerFactory.getLogger(EmailConsumer.class);
