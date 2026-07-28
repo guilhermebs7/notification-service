901836f313848c3a07c34e3bb7a475db53f90cb1
guilhermebs7 <gbs5@cin.ufpe.br>
2026-07-28 16:24:12 -0300

    feat: add notification DTOs

---
+++ src/main/java/br/notification_service/dto/UsuarioCriadoEvent.java
@@ -0,0 +1,4 @@
+package br.notification_service.dto;
+
+public record UsuarioCriadoEvent(Long id, String nome, String email) {
+}
---
+++ src/main/java/br/notification_service/dto/UsuarioRequest.java
@@ -0,0 +1,4 @@
+package br.notification_service.dto;
+
+public record UsuarioRequest(String nome, String email ) {
+}
